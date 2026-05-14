692e9e21d018ad5a2b2417d4c011714e643597f2
raulpra <raulprada@gmail.com>
2026-05-14 17:43:03 +0200

    first commit

---
+++ src/main/java/com/example/practicas_presenc/PracticasPresencApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicas_presenc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasPresencApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasPresencApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practicas_presenc/PracticasPresencApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicas_presenc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasPresencApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
